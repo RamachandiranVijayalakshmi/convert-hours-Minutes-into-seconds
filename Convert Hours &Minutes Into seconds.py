49a2af4399ef816bbcbbd1bb7d3d79b6861c8e28
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2020-06-17 23:34:50 +0530

    Add files via upload

---
+++ Convert Hours &Minutes Into seconds.py
@@ -0,0 +1,6 @@
+def function(Hours,minutes):
+    return (Hours*60*60+minutes*60)
+a=int(input('enter the Hour:'))
+b=int(input('enter the Minutes:'))
+print("total seconds for hour & Minute:",function(a,b))
+
